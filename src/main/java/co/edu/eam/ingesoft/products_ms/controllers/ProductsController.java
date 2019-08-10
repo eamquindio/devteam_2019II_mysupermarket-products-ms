7208c62f48ee20c8d8a57392c08a1cbb8f0ed97f
DanielaDiazP <dani092012@hotmail.com>
2019-08-09 21:45:24 -0500

    [resolves #10] methods created to be able to edit the products

--- src/main/java/co/edu/eam/ingesoft/products_ms/controllers/ProductsController.java
+++ src/main/java/co/edu/eam/ingesoft/products_ms/controllers/ProductsController.java
@@ -1,10 +1,26 @@
 package co.edu.eam.ingesoft.products_ms.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.edu.eam.ingesoft.products_ms.model.Person;
+import co.edu.eam.ingesoft.products_ms.model.Products;
+import co.edu.eam.ingesoft.products_ms.services.PersonService;
+import co.edu.eam.ingesoft.products_ms.services.ProductsService;
+
 @RestController
-@RequestMapping("api/products-ms/xxxx")
+@RequestMapping("api/products-ms/products")
 public class ProductsController {
+	
+	@Autowired
+	private ProductsService productsService;
+	
+	@PutMapping(value="/")
+	public void edit(@RequestBody Products products) {
+		productsService.update(products);
+	}
 
 }
--- src/main/java/co/edu/eam/ingesoft/products_ms/services/ProductsService.java
+++ src/main/java/co/edu/eam/ingesoft/products_ms/services/ProductsService.java
@@ -3,14 +3,17 @@ package co.edu.eam.ingesoft.products_ms.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.eam.ingesoft.products_ms.model.Products;
 import co.edu.eam.ingesoft.products_ms.repositories.ProductsRepository;
 
 @Service
 public class ProductsService {
 	
 	@Autowired
-	private ProductsRepository storesRepository;
+	private ProductsRepository productsRepository;
 	
-	
+	public void update(Products products) {
+		productsRepository.save(products);
+	}
 
 }
