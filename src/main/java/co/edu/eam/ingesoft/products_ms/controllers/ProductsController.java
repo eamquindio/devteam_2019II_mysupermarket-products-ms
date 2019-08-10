acc0e6a8966440e90e82e5bd0aea3c9b57d199d7
diegolt8 <diegoalugo@hotmail.com>
2019-08-10 08:40:55 -0500

    [resolves #9] resolves the errors in the code

--- src/main/java/co/edu/eam/ingesoft/products_ms/controllers/ProductsController.java
+++ src/main/java/co/edu/eam/ingesoft/products_ms/controllers/ProductsController.java
@@ -2,8 +2,8 @@ package co.edu.eam.ingesoft.products_ms.controllers;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import co.edu.eam.ingesoft.products_ms.model.Products;
-import co.edu.eam.ingesoft.products_ms.services.ProductsService;
 import org.springframework.web.bind.annotation.*;
+import co.edu.eam.ingesoft.products_ms.services.ProductsService;
 
 @RestController
 @RequestMapping("api/products-ms/products")
