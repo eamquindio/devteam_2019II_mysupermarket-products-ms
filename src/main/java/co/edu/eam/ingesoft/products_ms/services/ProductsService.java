5fd676aeeeea8bea49d1fbd326ab70ac2e75cb81
juan David <salgadohenao@gmail.com>
2019-08-19 21:57:10 -0500

    [resolves #7] products_category

--- src/main/java/co/edu/eam/ingesoft/products_ms/services/ProductsService.java
+++ src/main/java/co/edu/eam/ingesoft/products_ms/services/ProductsService.java
@@ -3,13 +3,13 @@ import java.util.List;
 import co.edu.eam.ingesoft.products_ms.model.Products;
 import co.edu.eam.ingesoft.products_ms.repositories.ProductsRepository;
 
-public class ProductsService {
+public class ProductsService{
 
 	private ProductsRepository productRepository;
 	/**
 	 * Method found by category.
-	 * @param category,data type String.
-	 * @return,by 'category' the products.
+	 * category,data type String.
+	 * return by 'category' the products.
 	 */
 	public List<Products> findByCategory(String category) {
 		return productRepository.findByCategory(category);
