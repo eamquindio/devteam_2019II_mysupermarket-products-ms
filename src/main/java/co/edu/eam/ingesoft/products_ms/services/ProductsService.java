1ef4d713023e43bf481f1355ffdafb32f924eefc
juan David <salgadohenao@gmail.com>
2019-08-19 21:15:58 -0500

    [resolves #7] products_category

--- src/main/java/co/edu/eam/ingesoft/products_ms/services/ProductsService.java
+++ src/main/java/co/edu/eam/ingesoft/products_ms/services/ProductsService.java
@@ -1,10 +1,9 @@
 package co.edu.eam.ingesoft.products_ms.services;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import co.edu.eam.ingesoft.products_ms.model.Products;
 import co.edu.eam.ingesoft.products_ms.repositories.ProductsRepository;
-@Service
+
 public class ProductsService {
 
 	@Autowired
