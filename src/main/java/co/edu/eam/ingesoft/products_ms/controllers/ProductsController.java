4934f4ebc11ff69f39b3184ba07e8bb97506af50
juan David <salgadohenao@gmail.com>
2019-08-19 20:51:51 -0500

    [resolves #7] products_category

--- src/main/java/co/edu/eam/ingesoft/products_ms/controllers/ProductsController.java
+++ src/main/java/co/edu/eam/ingesoft/products_ms/controllers/ProductsController.java
@@ -1,24 +1,19 @@
 package co.edu.eam.ingesoft.products_ms.controllers;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import co.edu.eam.ingesoft.products_ms.model.Products;
+import org.springframework.web.bind.annotation.*;
 import co.edu.eam.ingesoft.products_ms.services.ProductsService;
 
-/**
- * Products controller.
- * @author caferrerb
- *
- */
 @RestController
 @RequestMapping("api/products-ms/products")
 public class ProductsController {
-
-  /**
-   * product service.
-   */
-  @Autowired
-  private ProductsService productsService;
-
+	@Autowired
+	private ProductsService productsService;
+	// value find_by_category
+	@GetMapping(value="/find_by_category")
+		public List<Products> findByCategory(@RequestParam String category) {
+		   System.out.println(category);//print on screen the category
+		   return productsService.findByCategory(category);
+			}	
 }
