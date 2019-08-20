7fa7d5544ca89ff7a2868210e334c58d22192861
juan David <salgadohenao@gmail.com>
2019-08-19 21:49:15 -0500

    [resolves #7] products_category

--- src/main/java/co/edu/eam/ingesoft/products_ms/controllers/ProductsController.java
+++ src/main/java/co/edu/eam/ingesoft/products_ms/controllers/ProductsController.java
@@ -1,12 +1,10 @@
 package co.edu.eam.ingesoft.products_ms.controllers;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import co.edu.eam.ingesoft.products_ms.model.Products;
 import org.springframework.web.bind.annotation.*;
 import co.edu.eam.ingesoft.products_ms.services.ProductsService;
 @RequestMapping("api/products-ms/products")
 public class ProductsController {
-	@Autowired
 	private ProductsService productsService;
 	
 	@GetMapping(value="/find_by_category")
