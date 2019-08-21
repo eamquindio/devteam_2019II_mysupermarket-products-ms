b8e42f794e389cff555033ea009778154bfb3ac3
Camilo Andres Ferrer Bustos <camilo.ferrer@rappi.com>
2019-08-21 08:37:10 -0500

    fix styles

--- src/main/java/co/edu/eam/ingesoft/products_ms/controllers/ProductsController.java
+++ src/main/java/co/edu/eam/ingesoft/products_ms/controllers/ProductsController.java
@@ -14,8 +14,8 @@ import co.edu.eam.ingesoft.products_ms.model.Products;
 import co.edu.eam.ingesoft.products_ms.services.ProductsService;
 
 /**
- * REst controller for products entity.
- * Products controller.
+ * REst controller for products entity. Products controller.
+ *
  * @author caferrerb
  */
 
@@ -27,43 +27,49 @@ public class ProductsController {
   /**
    * products service.
    */
-@Autowired
-private ProductsService productsService;
+  @Autowired
+  private ProductsService productsService;
+
   /**
    * list all products.
    *
    * @return list of all products
    */
-@GetMapping(value = "/all")
-public List<Products> findAll() {
-return productsService.listAll();
-}
-/**
- * list products to category.
- * @param category category
- * @return list to products
- */
-@GetMapping(value = "/find_by_category")
-public List<Products> findByCategory(@RequestParam String category) {
-return productsService.findByCategory(category);
-}
+  @GetMapping(value = "/all")
+  public List<Products> findAll() {
+    return productsService.listAll();
+  }
+
+  /**
+   * list products to category.
+   *
+   * @param category category
+   * @return list to products
+   */
+  @GetMapping(value = "/find_by_category")
+  public List<Products> findByCategory(@RequestParam String category) {
+    return productsService.findByCategory(category);
+  }
+
   /**
    * Edit a products.
+   *
    * @param product product to edit
    */
   @PutMapping(value = "/")
   public void edit(@RequestBody Products product) {
     productsService.update(product);
   }
-/**
-* Method found by name.
-* @param name name
-* @return list of products
-*/
-@GetMapping(value = "/find_by_name")
-public List<Products> findByName(@RequestParam String name) {
-System.out.println(name);
-return productsService.findByName(name);
-}
-}
 
+  /**
+   * Method found by name.
+   *
+   * @param name name
+   * @return list of products
+   */
+  @GetMapping(value = "/find_by_name")
+  public List<Products> findByName(@RequestParam String name) {
+    System.out.println(name);
+    return productsService.findByName(name);
+  }
+}
--- src/main/java/co/edu/eam/ingesoft/products_ms/repositories/ProductsRepository.java
+++ src/main/java/co/edu/eam/ingesoft/products_ms/repositories/ProductsRepository.java
@@ -1,4 +1,5 @@
 package co.edu.eam.ingesoft.products_ms.repositories;
+
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
@@ -7,28 +8,33 @@ import co.edu.eam.ingesoft.products_ms.model.Products;
 
 /**
  * Product repository.
+ *
  * @author caferrerb
  *
  */
 @Repository
 public interface ProductsRepository extends CrudRepository<Products, String> {
 
-/**
-  * Find all persons.
-  * @return list or persons.
-  */
-List<Products> findAll();
- /**
- * Method that list the products by category.
- * @param category data type String.
- * @return the products by their category.
- */
-List<Products> findByCategory(String category);
-/**
-* Find all products given a name.
-* @param name name to look for.
-* @return list or products.
-*/
-List<Products> findByName(String name);
-}
+  /**
+   * Find all persons.
+   *
+   * @return list or persons.
+   */
+  List<Products> findAll();
 
+  /**
+   * Method that list the products by category.
+   *
+   * @param category data type String.
+   * @return the products by their category.
+   */
+  List<Products> findByCategory(String category);
+
+  /**
+   * Find all products given a name.
+   *
+   * @param name name to look for.
+   * @return list or products.
+   */
+  List<Products> findByName(String name);
+}
