2cabfc0c12364a6c0577cae37ea0827c14092590
Camilo Andres Ferrer Bustos <camilo.ferrer@rappi.com>
2019-08-23 22:01:18 -0500

    unit test

--- src/main/java/co/edu/eam/ingesoft/products_ms/controllers/PersonController.java
+++ src/main/java/co/edu/eam/ingesoft/products_ms/controllers/PersonController.java
@@ -2,7 +2,11 @@ package co.edu.eam.ingesoft.products_ms.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.eam.ingesoft.products_ms.model.Person;
+import co.edu.eam.ingesoft.products_ms.routes.Router;
 import co.edu.eam.ingesoft.products_ms.services.PersonService;
 
 /**
@@ -23,7 +28,7 @@ import co.edu.eam.ingesoft.products_ms.services.PersonService;
  *
  */
 @RestController
-@RequestMapping("api/products-ms/person")
+@RequestMapping(Router.PERSON_PATH)
 public class PersonController {
 
   /**
@@ -36,41 +41,51 @@ public class PersonController {
    * create a person operation.
    *
    * @param person person to create
+   * @return person created
    */
-  @PostMapping(value = "/")
-  public void create(@RequestBody Person person) {
-    personService.create(person);
+  @PostMapping(value = Router.CREATE_PERSON)
+  public Person create(@RequestBody Person person) {
+    return personService.create(person);
   }
 
   /**
    * find a person.
    *
    * @param id id for person to find
+   * @param response httpresponse
    * @return person with id
    */
-  @GetMapping(value = "/{id}")
-  public Person find(@PathVariable Integer id) {
-    return personService.find(id);
+  @GetMapping(value = Router.FIND_PERSON + "/{id}")
+  public Person find(@PathVariable Integer id, HttpServletResponse response) {
+    Person person = personService.find(id);
+
+    if (person == null) {
+      response.setStatus(HttpStatus.NOT_FOUND.value());
+    }
+
+    return person;
   }
 
   /**
    * Delete a person.
    *
    * @param id id person to delete
+   * @return person deleted
    */
-  @DeleteMapping(value = "/{id}")
-  public void delete(@PathVariable Integer id) {
-    personService.delete(id);
+  @DeleteMapping(value = Router.DELETE_PERSON + "/{id}")
+  public Person delete(@PathVariable Integer id) {
+    return personService.delete(id);
   }
 
   /**
    * Edit a person.
    *
    * @param person perso to edit
+   * @return person edited
    */
-  @PutMapping(value = "/")
-  public void edit(@RequestBody Person person) {
-    personService.update(person);
+  @PutMapping(value = Router.EDIT_PERSON)
+  public Person edit(@RequestBody Person person) {
+    return personService.update(person);
   }
 
   /**
@@ -79,19 +94,31 @@ public class PersonController {
    * @param name name person to find
    * @return list of person with a name
    */
-  @GetMapping(value = "/find_by_name")
-  public List<Person> findByName(@RequestParam String name) {
-    System.out.println(name);
-    return personService.findByName(name);
+  @GetMapping(value = Router.FIND_BY_NAME)
+  public ResponseEntity<List<Person>> findByName(@RequestParam String name) {
+    List<Person> persons = personService.findByName(name);
+
+    if (persons.isEmpty()) {
+      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    return new ResponseEntity<>(persons, HttpStatus.OK);
   }
 
   /**
    * list all persons.
    *
+   * @param response httpresponse
    * @return list of all persons
    */
-  @GetMapping(value = "/all")
-  public List<Person> findAll() {
-    return personService.listAll();
+  @GetMapping(value = Router.FIND_ALL)
+  public List<Person> findAll(HttpServletResponse response) {
+    List<Person> persons = personService.listAll();
+
+    if (persons.isEmpty()) {
+      response.setStatus(HttpStatus.NO_CONTENT.value());
+    }
+
+    return persons;
   }
 }
